e2a3853e83e6d2efcb1af0f34765781ec1257294
PrathmTayade <122280972+PrathmTayade@users.noreply.github.com>
2023-05-06 02:11:33 +0530

    auth completed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import Feed from "@components/Feed";
 
 const Home = () => (
   <section className="w-full flex-center flex-col">
@@ -10,6 +11,7 @@ const Home = () => (
       Promptopia is an open-source AI prompting tool for modern world to
       discover, create and share creative prompts
     </p>
+    <div className=""></div>
 
     <Feed />
   </section>
